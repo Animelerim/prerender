b74e32a6526389e1fbd9b175d79408ec552d167a
Üsame Fethullah AVCI <u@gaaraj.com>
2017-06-29 17:04:38 +0300

    development batch file created

---
+++ start.sh
@@ -0,0 +1,14 @@
+export NODE_ENV=production;
+export SERVER_HOSTNAME=localhost;
+#export SERVER_PORT=80;
+export NUM_WORKERS=1;
+export NUM_ITERATIONS=100;
+export NUM_SOFT_ITERATIONS=100;
+export ALLOWED_DOMAINS=;
+
+export AWS_ACCESS_KEY_ID=;
+export AWS_SECRET_ACCESS_KEY=;
+export AWS_S3_BUCKET_NAME=;
+export AWS_REGION=;
+
+nodemon server.js;
